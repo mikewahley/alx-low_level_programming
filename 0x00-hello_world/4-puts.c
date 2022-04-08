bdc6e2ba9c32f47662946f96a260f5df9b9e1fbe
Ad3w4L3_A <mikewahley@gmail.com>
2022-04-08 19:10:18 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/** 
-  * main - This is a c proram that prints using puts
-  * return - return a new line
-  */
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero if otherwise
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
